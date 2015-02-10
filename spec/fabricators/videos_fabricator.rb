3a558fcafc57440484a1b18682751040c7a7b46d
Tim K <tim750233@gmail.com>
2015-02-10 15:44:46 +0000

    halfway done with queue spec

--- spec/fabricators/videos_fabricator.rb
+++ spec/fabricators/videos_fabricator.rb
@@ -1,5 +1,4 @@
 Fabricator(:video) do
   title { Faker::Lorem.words(5).join(' ') }
   description { Faker::Lorem.paragraph(2) }
-  small_cover_url { "#{title}.jpg " }
 end
--- spec/features/queue_workflow_spec.rb
+++ spec/features/queue_workflow_spec.rb
@@ -2,27 +2,41 @@ require 'spec_helper'
 
 feature 'full queue workflow' do
   
-  given(:user) { Fabricate(:user) }
-  given(:video1) { Fabricate(:video) }
+  given!(:user) { Fabricate(:user) }
+  given!(:cat1) { Fabricate(:category) }
+  given!(:video1) { Fabricate(:video, category: cat1) }
 
-  scenario 'user signs in with correct credentials' do
+  scenario 'user clicks a video on the homepage and ends up on the correct video page' do
+    sign_in
+    add_video_to_queue(video1)
+    expect(page).to have_content(video1.title)
+  end
+
+  scenario 'user adds video to queue' do
+    sign_in
+    add_video_to_queue(video1)
+    click_link('+ My Queue')
+    expect(page).to have_content(video1.title)
+  end
+
+  scenario 'user clicks video link on the queue page' do
+    sign_in
+    add_video_to_queue(video1)
+    click_link('+ My Queue')
+    click_link(video1.title)
+    save_and_open_page
+    expect(page).to have_content(video1.description)
+    expect(page).to have_no_content('+ My Queue')
+  end
+
+  def sign_in
     visit '/login'
     fill_in :email, with: user.email
     fill_in :password, with: user.password
     click_button 'Sign In'
-    expect(page).to have_content('My Queue')
   end
 
-  scenario 'user cannot sign in with incorrect credentials' do
-    visit '/login'
-    fill_in :email, with: user.email
-    fill_in :password, with: 'wrong password'
-    click_button 'Sign In'
-    expect(page).to_not have_content('My Queue')
-  end
-
-  scenario 'user clicks a video on the homepage' do
-    visit '/home'
-    save_and_open_page
+  def add_video_to_queue(video)
+    click_link("video_#{video.id}_link")
   end
 end
