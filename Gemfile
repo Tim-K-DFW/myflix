cda5cabbc2e3e0b9277047bed3c71ef11469f240
Tim K <tim750233@gmail.com>
2015-03-25 23:46:35 +0000

    added feature spec for password reset

--- Gemfile
+++ Gemfile
@@ -33,6 +33,7 @@ end
 group :test do
   gem 'database_cleaner', '1.2.0'
   gem 'capybara'
+  gem 'capybara-email'
 end
 
 group :production do
--- spec/support/macros.rb
+++ spec/support/macros.rb
@@ -23,3 +23,14 @@ end
 def click_video_on_homepage(video)
   click_link("video_#{video.id}_link")
 end
+
+def reset_password_flow(user)
+  visit '/login'
+  click_link('Forgot password?')
+  fill_in :email, with: user.email
+  click_button 'Send Email'
+  email = open_email(user.email)
+  email.click_link('Reset your password')
+  fill_in :password, with: '123'
+  click_button('Reset Password')
+end
