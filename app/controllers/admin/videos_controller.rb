f2743e7df8778a5f951cfc81abc13668561fff69
Tim K <tim750233@gmail.com>
2015-04-02 14:23:49 +0000

    almost done with upload to local

--- app/controllers/admin/videos_controller.rb
+++ app/controllers/admin/videos_controller.rb
@@ -4,5 +4,25 @@ class Admin::VideosController < ApplicationController
   def new
     @video = Video.new
   end
+
+  def create
+    @video = Video.new(video_params)
+    if @video.valid?
+      @video.small_cover_url = @video.small_cover_upload.filename
+      @video.large_cover_url = @video.large_cover_upload.filename
+      @video.save
+      flash[:success] = 'Video was added successfully.'
+      redirect_to home_path
+    else
+      flash[:danger] = 'There was an error. Try again.'
+      render :new
+    end
+  end
+
+  private
+
+  def video_params
+    params.require(:video).permit(:category_id, :description, :title, :small_cover_upload, :large_cover_upload)
+  end
   
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150401001057) do
+ActiveRecord::Schema.define(version: 20150402140503) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -70,6 +70,8 @@ ActiveRecord::Schema.define(version: 20150401001057) do
     t.integer  "category_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "small_cover_upload"
+    t.string   "large_cover_upload"
   end
 
 end
