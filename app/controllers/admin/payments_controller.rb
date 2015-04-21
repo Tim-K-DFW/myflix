ee6a005da367534c00c06eabceea000201e468db
Tim K <tim750233@gmail.com>
2015-04-21 12:47:03 +0000

    payments view completed

--- app/controllers/admin/payments_controller.rb
+++ app/controllers/admin/payments_controller.rb
@@ -1,4 +1,7 @@
-class PaymentsController < ApplicationController
+class Admin::PaymentsController < ApplicationController
+  before_action :require_admin
+
   def index
+    @payments = Payment.all
   end
 end
